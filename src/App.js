1235088d5131df95b1f053e9efab06d91bba486a
Joachim Byrnes-Shay <me@joachimbyrnesshay.com>
2022-11-17 03:40:43 -0500

    some commenting added, in progress

--- src/App.js
+++ src/App.js
@@ -9,14 +9,21 @@ import { useState, useEffect, useRef } from 'react';
 // ! reevaluate how many times I am calling setCurrecyData and where
 // ! rename functions and variables appropriately, evaluate 
 
+
+// url and app_id for exchangerates provider, to be utilized in fetchAll method.
 const FetchConstants = {
      baseURL: 'https://openexchangerates.org/api/',
      app_id: '0f6288f8f4b4421ba1a18cf74a5b9dcf'
 }
 
+// limit on maximum num of comparison currencies, to compare with base currency, dictates the quantity of charts which will display and 
+// other UI behavior signaling to user such as the threshhold at which flash message alerts user that no further currencies can be 
+// added to comparisons list without removing one or more
 const MaxNumOfComparisons = 7;
 
 function App() {
+    // useRef is utilized solely to ease the control of shifts of focus from filter fields to select boxes, primarily for the implementation
+    // of the capability to navigate via arrowDown from filter input to select, or via arrowUp from select box to filter input
     const Refs = {
         baseFilter: useRef(null),
         convertFilter: useRef(null),
@@ -24,33 +31,45 @@ function App() {
         convertSelect: useRef(null),
     }
   
+    // storing of indices as previous values aids the control of visual and functional navigation from index 0 on select lists into filter input (focus in filter input) via onKeyUp condition regarding arrowUp
     const [prevBaseIndex, setPrevBaseIndex] = useState(-1);
     const [prevConvertIndex, setPrevConvertIndex] =useState(-1);
+
+    // all charts have an associated modal element which will be hidden at first, show on mouseclick on chart element, then visible modals will disappear with animation upon mouseleave of the element
+    // all of which is triggered by changes in the below state variables.  default state variable value is an array large enough for maximum num comparision currencies + the base value chart which will be displayed along with them
     const [isChartModalDisplayed, setIsChartModalDisplayed] =useState(Array(MaxNumOfComparisons+1).fill(false));
     const [isChartModalAnimatingDisappearance, setIsChartModalAnimatingDisappearance] = useState(Array(MaxNumOfComparisons+1).fill(false));
 
+    // onChange of each select list, value is set appropriately on select element itself.
     const [baseSelectValue,setBaseSelectValue] = useState(undefined);
     const [convertSelectValue,setConvertSelectValue] = useState(undefined);
 
+    // flash message is displayed when user attempts to exceed 1 more than the allowed maximum number of comparison/conversion currency selections.  whether flash is displayed is controlled by boolean value of state variable.
     const [isFlashDisplayed, setIsFlashDisplayed] = useState(false);
 
+    // boolean value stored in isSmallScreen controls the dimension of the charts, i.e. width or height, which is dependant upon screen size, 
+    // controlled via useEffect call with isSmallScreen in dependency array as seen below
     const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+    // control filter UI (display of text input) for both base filter and convert/comparisons filter
     const [baseFilterVal, setBaseFilterVal] = useState("");
     const [convertFilterVal, setConvertFilterVal] = useState("");
 
-
+    // default base currency is convertFrom, default comparisons are the standardized currency abbreviations as strings in convertTo.   These values can be changed via UI.  
     const [currencySelections, setCurrencySelections] = useState({
         convertFrom: 'USD',
         convertTo: ['AED', 'BGN', 'CNY', 'EUR', 'GBP'],
     });
 
+    // currencyInfo rates are pulled from the API, and will be updated via useEffect when convertFrom changes, as rates requested are values relative to the base currency provided by app via the call to the API
+    //  fullNames are the full textual names such as United States Dollar to match the abbreviations for currency such as USD.  These names are used in the informative text displayed on mdoals and in tooltips 
+    // upon hover over the display of current base and comparison configuration values in the top configuration section of the UI.
     const [currencyInfo, setCurrencyInfo] = useState({
         rates: {},
         fullNames: {},
     });
 
-  
-
+    // selected API requires 2 separate URLS to get the desired data as above, one for rates, one with the full names, thus 2 fetches which are chained and nested so that ALL info is ready upon first render
     function fetchAll() {
         let baseRates = `latest.json?app_id=${FetchConstants.app_id}&base='${currencySelections.convertFrom}'`;
         let baseSubURLfullNames = 'currencies.json';
@@ -66,18 +85,30 @@ function App() {
             );
     }
 
+
     const GenUtils = {
+        // simple utility method to alphabetize string arrays, which sorts what is purposefully a copy of any array passed in, and thus will avoid mutating the original argument
+        // utilized to alphabetize the names of selected comparison currencies upon changes, which is then provided as the new value of convertFrom via state
+        // on re-renders this allows a continuity of a sensible order of the charts displayed (comparisons only) as well as of the visual display of the current selected comparison currs found in top right config section
         alphabetizeStringArr: (arr)=> {
-            let arr2 = [...arr].sort();
-            return arr2;
+            let arrayCopy = [...arr].sort();
+            return arrayCopy;
         }
     }
 
     const ChartsUtils = {
+        // the charts displayed depend upon inserting the convertFrom value to the left of all other charts
         getChartsCurrencies: ()=>[currencySelections.convertFrom, ...currencySelections.convertTo],
-        getChartsCurrenciesWithSize: ()=>ChartsUtils.getChartsCurrencies().map(value=>{
-            return {currency: value, size: ChartsUtils.getIndividualChartSize(value)}
+        
+        // to avoid duplicated calls per each required chart of calculation of size of chart from the JSX chart creation code, upon which calculated value various different attributes within creation of the charts elements are dependant 
+        // the array of currency names from which charts will be made are mapped to an array of objects with the name and the 'size' required for the chart, dependant upon call to getIndividualChartSize(name)
+        // the result is then the array that is utilized in JSX for chart element creation
+        getChartsCurrenciesWithSize: ()=>ChartsUtils.getChartsCurrencies().map(name=>{
+            return {currency: name, size: ChartsUtils.getIndividualChartSize(name)}
         }),
+        // the largest float value among selected currency rates will be represented as the largest graph.  e.g. if 7 CNY is largest value among comparisons to 1 USD base, CNY will be largest graph,
+        // USD base will be approximate 1/7 its height as a graph, and other currencies will fall whereever is appropriate, less than or more than USD in size but no larger than CNY
+        // if 1 unit of USD is larger than all selected comparisons (because they are some decimal portion less than 1), USD will bhe largest
         getMaxUnitsOfChartCurrs: ()=> {
             let max = currencySelections.convertFrom;
             ChartsUtils.getChartsCurrencies().forEach(value=>{
@@ -87,43 +118,45 @@ function App() {
             })
             return max;
         },
+        // utilzing the selected currency which has the largest units in comparison to 1 unit of whichever base currency, the size of any chart will be converted to a proporption of 
+        // the maximum (i.e. 100% height or width of the charts area display)
         getIndividualChartSize: (value)=> {
             return parseFloat(currencyInfo.rates[value]) / parseFloat(currencyInfo.rates[ChartsUtils.getMaxUnitsOfChartCurrs()]) * 100
         },
+        // dependant upon isSmallScreen boolean state variable which will change upon window resize listener to true (small) or false (large) the orientation value will be used in inline style of JSX
+        // to change charts to horizontal (width) or vertical(height) accordingly
         getChartsOrientation: ()=> isSmallScreen ? 'width' : 'height',
     
     }
 
     const FilterHandling = {
-       
-        handleBaseFilterChange: (thisFilter)=> {
-            // the filter field input, used as new value in JSX upon changes
+        // the value of base filter field input, used as new value in JSX upon changes
+        handleBaseFilterChange: (thisFilter)=> { 
             let val = thisFilter.target.value;
-     
             setBaseFilterVal(val);
         },
+        // the value of convert/comparison filter field input
         handleConvertFilterChange(thisFilter) {
             let val = thisFilter.target.value;
             setConvertFilterVal(val);
         },
-
+        // control of focus, arrowDown while in base filter field allows use to navigate seamlessly into first index of base select list as if it were all one element
         handleBaseFilterDownArrowToSelect(e) {
+            // 40 is arrowDown
             if (e.keyCode === 40){
-                console.log("should go to select");
                 Refs.baseSelect.current.focus();
-                /// manually set index because otherwise the select element is focused but index is undefined at first,
-                // requiring 2 down arrow key hits to get to index 0 instead of 1.
+                // manually set index to 0 (first) because otherwise the select element is focused but index is undefined at first,
+                // which a consequence that the user is required to use 2 downarrow key hits to get to index 0 instead of 1.
                 Refs.baseSelect.current.selectedIndex = 0;
             }
         },
+        // control of focus, re arrowDownfrom conversion currencies filter input field to conversion currencieds select list, as above
         handleConvertFilterDownArrowToSelect(e) {
-            if(e.keyCode === 40) {
-                console.log("should go to select");
+            if (e.keyCode === 40) {
                 Refs.convertSelect.current.focus();
                 Refs.convertSelect.current.selectedIndex = 0;
             }
         }
-    
     }
 
     const SelectHandling = {
@@ -183,6 +216,7 @@ function App() {
              newArr = GenUtils.alphabetizeStringArr(newArr);
              setCurrencySelections({ ...currencySelections, convertTo: newArr})
          }
+         setPrevConvertIndex(ix);
      },
         handleOptionClick_base: (optionVal, e)=> {
             // console.log(e.target.textContent);
